cc7d8148b458990b17ad6bd172388004364ea8d0
shohagkhan <4shohag@gmail.com>
2019-09-14 03:33:53 +0600

    Basic
    Basic 4 steps of Redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { createStore } from "redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+
+    const reducer = (state = {}, action) => {
+      if (action.type === 'A') {
+        return { A: 'I am A' }
+      }
+      if (action.type==='B') {
+        return {
+          ...state,
+          B: 'I am B'
+        }
+      }
+      return state
+    }
+
+    const store = createStore(reducer)
+
+    store.subscribe(() => {
+      console.log(store.getState().A)
+    })
+    store.subscribe(() => {
+      console.log(store.getState().B)
+    })
+
+    
+    store.dispatch({ type: 'A' })
+    store.dispatch({ type: 'B' })
+
+
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+// 1. reducer 
+// 2. store 
+// 3. subscribe 
+// 4. Dispatch
