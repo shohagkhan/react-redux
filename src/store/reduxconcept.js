32fe57d56958fa599656e14a0ccbf4e01fa372a2
shohagkhan <4shohag@gmail.com>
2019-09-15 04:18:48 +0600

    To Do
    Working Steps

---
+++ src/store/reduxconcept.js
@@ -0,0 +1,22 @@
+// store
+//     Action 
+//     Reducer 
+
+
+// React Components
+//     Child Components 
+//         Child Components 
+
+
+// React-Redux  Connect 
+//     Provider Component 
+//         Store 
+//         App Component
+
+// If we need data 
+// mapStateToProps(state) return Object 
+
+// if we want to dispatch some action 
+// mapDispatchToProps(dispatch) return object
+
+// connect(mapStateToProps, mapDispatchToProps)(components)
